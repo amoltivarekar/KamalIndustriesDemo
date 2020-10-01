b01c1ed832b47f873980517b29b63edbc3e433d9
Amol Tivarekar <amoltivarekar@gmail.com>
2020-10-01 17:07:28 +0530

    Add files via upload

--- js/custom.js
+++ js/custom.js
@@ -1,21 +1,22 @@
 $(document).ready(function(){
   //check if window scroll is < 100
   if($(window).scrollTop() < 100){
-       $('#pageUp').fadeOut().hide();
+    $('#pageUp').fadeOut().hide();
   }
 
   //Check to see if the window is top if not then display button
   $(window).scroll(function(){
-      if ($(this).scrollTop() > 100) {
-          $('#pageUp').show().fadeIn();
-      } else {
-          $('#pageUp').fadeOut().hide();
-      }
+    if ($(this).scrollTop() > 100) {
+      $('#pageUp').show().fadeIn();
+    } else {
+      $('#pageUp').fadeOut().hide();
+    }
   });
 
   //Click event to scroll to top
   $('#pageUp a').click(function(){
-      $('html, body').animate({scrollTop : 0},360);
-      return false;
+    $('html, body').animate({scrollTop : 0},360);
+    return false;
   });
-})
+  
+});
